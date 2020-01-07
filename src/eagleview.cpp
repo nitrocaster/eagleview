5c1e2302c249de417fd8992f1522b5a649cb9a3f
Pavel Kovalenko <su@nitrocaster.me>
2020-01-07 13:56:32 +0300

    Support multiple input formats.

--- src/eagleview.cpp
+++ src/eagleview.cpp
@@ -3,37 +3,85 @@
 
 #include "ToptestBoardview.hpp"
 #include "ToptestEagleImporter.hpp"
+#include "ToptestTeboImporter.hpp"
 #include "ToptestWriter.hpp"
 #include <cstdio> // std::puts
 #include <fstream> // std::ofstream
 
 static void PrintUsage()
 {
-    puts("usage: eagleview <eagle brd file name> {-brd} <output file name>");
+    puts("usage: eagleview <input format> <input path> <output format> <output path>\n"
+        "supported input formats:\n"
+        "[-eagle] Autodesk EAGLE board (*.BRD)\n"
+        "[-tebo] Tebo-ICT view (*.TVW)\n"
+        "supported output formats:\n"
+        "[-toptest] Toptest board view (*.BRD)");
 }
+
+using ImporterFunction = bool(*)(std::string const &srcPath, Toptest::Boardview &brd);
+
+static bool ImportEagle(std::string const &srcPath, Toptest::Boardview &brd)
+{
+    using namespace tinyxml2;
+    XMLDocument src;
+    if (src.LoadFile(srcPath.c_str()) != XML_SUCCESS)
+    {
+        printf("! %s\n", src.ErrorStr());
+        return false;
+    }
+    Toptest::EagleImporter importer(brd);
+    importer.Import(src);
+    return true;
+}
+
+static bool ImportTebo(std::string const &srcPath, Toptest::Boardview &brd)
+{
+    auto fs = std::ifstream(srcPath, std::ios::binary);
+    Tebo::Board src;
+    src.Load(fs);
+    Toptest::TeboImporter importer(brd);
+    importer.Import(src);
+    return true;
+}
+
+static std::map<std::string, ImporterFunction> Importers
+{
+    {"-eagle", ImportEagle},
+    {"-tebo", ImportTebo}
+};
 
 int main(int argc, char const *argv[])
 {
-    if (argc != 4)
+    if (argc != 5)
     {
         PrintUsage();
         return 1;
     }
-    auto srcFile = argv[1];
-    auto targetFormat = argv[2];
-    auto dstFile = argv[3];
-    using namespace tinyxml2;
-    XMLDocument src;
-    if (src.LoadFile(srcFile) != XML_SUCCESS)
+    std::string srcFormat = argv[1],
+        srcPath = argv[2],
+        dstFormat = argv[3],
+        dstPath = argv[4];
+    if (dstFormat != "-toptest")
     {
-        printf("! %s\r\n", src.ErrorStr());
+        puts("! Unrecognized output format");
         return 1;
     }
     // XXX: catch exceptions
     Toptest::Boardview brd;
-    auto fs = std::ofstream(dstFile);
-    Toptest::EagleImporter importer(brd);
-    importer.Import(src);
+    auto importerIt = Importers.find(srcFormat);
+    if (importerIt == Importers.end())
+    {
+        puts("! Unrecognized input format");
+        return 1;
+    }
+    if (!importerIt->second(srcPath, brd))
+        return 1;
+    auto fs = std::ofstream(dstPath);
+    if (!fs)
+    {
+        printf("! Can't write '%s'\n", dstPath.c_str());
+        return 1;
+    }
     Toptest::Writer writer(brd);
     writer.Write(fs);
     return 0;
