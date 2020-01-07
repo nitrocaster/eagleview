0536835483569827cbfba4eb8d0615daee32c2b5
Pavel Kovalenko <su@nitrocaster.me>
2020-01-07 14:50:36 +0300

    R_ASSERT: Show detailed info when failed.

--- src/Common.hpp
+++ src/Common.hpp
@@ -1,3 +1,22 @@
 #pragma once
 
-#define R_ASSERT(expr) do { if (!(expr)) __debugbreak(); } while (0)
+#include <cstdio>
+
+inline void Fail(char const *expr, char const *func,
+    char const *file, int line, char const *desc)
+{
+    printf("\n! FATAL ERROR\n"
+        "  Expression: %s\n"
+        "  Function: %s\n"
+        "  File: %s:%d\n"
+        "  Description: %s\n",
+        expr, func, file, line, desc);
+    __debugbreak();
+}
+
+#define R_ASSERT(expr) \
+    do \
+    { \
+        if (!(expr)) \
+            Fail(#expr, __FUNCTION__, __FILE__, __LINE__, "assertion failed"); \
+    } while (0)
